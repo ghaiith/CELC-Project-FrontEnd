03148907733fad7e43758d36c2a55b3c9d153a29
Ghaith Ahmed <g.al3raqi@gmail.com>
2021-01-26 21:56:55 +0300

    add files

---
+++ script.js
@@ -0,0 +1,7 @@
+ocument.getElementById("saved").classList.add('fas');
+
+document.getElementById("saved").classList.remove('far');
+
+if ( document.getElementById("saved").classList.contains('MyClass') )
+
+document.getElementById("MyElement").classList.toggle('MyClass');
