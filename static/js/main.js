e4707644134f9963e9bbbe1cffe712294038ec4e
Ghaith Ahmed <g.al3raqi@gmail.com>
2021-01-27 20:05:31 +0300

    add new frame work to make some effects on ui

---
+++ static/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        rtl:true,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:1,
+            }
+        }
+    })
+  });
